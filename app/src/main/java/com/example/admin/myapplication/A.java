2968425b2029c82cecfc8e47eb8962ef1280e8aa
YOURUSERNAME <584513780@qq.com>
2019-04-10 09:06:05 +0800

    第4次提交

--- app/src/main/java/com/example/admin/myapplication/A.java
+++ app/src/main/java/com/example/admin/myapplication/A.java
@@ -1,4 +1,55 @@
 package com.example.admin.myapplication;
 
 public class A {
+   public static int count=0;
+   public static void main(String args[]){
+       (new Thread(){
+           @Override
+           public void run(){
+               System.out.println("hello world1");
+               while(true){
+                   try {
+                       Thread.sleep(1000);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+                   count++;
+                   System.out.println("又过去一辆车");
+               }
+           }
+       }).start();
+
+       (new Thread(){
+           @Override
+           public void run(){
+               while (true){
+                   try {
+                       Thread.sleep(1000);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+                   if(count!=0) {
+                       System.out.println("过去的车辆数=>" + count);
+                       count = 0;
+                   }
+               }
+
+           }
+       }).start();
+   }
+   synchronized static void printCar() {
+       if (count!=0) {
+           System.out.println("过去的车辆数=>" + count);
+           try {
+               Thread.sleep(1000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+           count = 0;
+       }
+   }
+   synchronized static void countCar(){
+       count++;
+       System.out.println("又过去一辆车");
+   }
 }
--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -3,7 +3,6 @@ package com.example.admin.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,42 +13,45 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     private TextView textView;
     private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.textView = (TextView) findViewById(R.id.abc);
-        this.button = (Button) findViewById(R.id.button1);
+        setContentView(R.layout.activity_main2);
+        this.textView = (TextView) findViewById(R.id.abcd);
+        this.button = (Button) findViewById(R.id.button);
         this.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                startActivity(new Intent(Main2Activity.this, MainActivity.class));
             }
         });
-
-        String weatherId = "CN101020200";
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=f691985632d949abb8df27de3516d2f3";
-        HttpUtil.sendOkHttpRequest(weatherUrl,new Callback(){
+        String weatherUrl = "http://guolin.tech/api/china";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.v("hello","world");
+
             }
 
             @Override
-            public void onResponse(Call call, Response response)throws IOException {
+            public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         textView.setText(responseText);
                     }
                 });
+
             }
+
+
         });
+
     }
+
 }
