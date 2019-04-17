9b85148c39e4800e3d035a46d493e326a3abcf80
YOURUSERNAME <584513780@qq.com>
2019-04-17 09:27:16 +0800

    第6次提交

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.admin.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -24,16 +26,18 @@ import okhttp3.Response;
 public class Main2Activity extends AppCompatActivity {
 
     private List<String> data2=new ArrayList();
-    private String[] data={"北京","浙江","江西"};
+    private String[] data={"北京","浙江","江西","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
     private TextView textView;
     private Button button;
     private ListView listView;
+//    private String[] data=new String[34];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         this.textView = (TextView) findViewById(R.id.abcd);
         this.button = (Button) findViewById(R.id.button);
+
         this.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,17 +47,20 @@ public class Main2Activity extends AppCompatActivity {
         this.listView = (ListView)findViewById(R.id.listview);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
         listView.setAdapter(adapter);
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("点击了哪一个",""+position);
+            }
+        });
         String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
                 String[] result=parseJSONObject(responseText);
+                Main2Activity.this.data=result;
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -62,26 +69,27 @@ public class Main2Activity extends AppCompatActivity {
                 });
 
             }
+            @Override
+            public void onFailure(Call call, IOException e) {
 
-            private String[] parseJSONObject(String responseText) {
-                JSONArray jsonArray= null;
-                try {
-                    jsonArray = new JSONArray(responseText);
-                    String[] result=new String[jsonArray.length()];
-                    for (int i=0;i<jsonArray.length();i++){
-                        JSONObject jsonObject=jsonArray.getJSONObject(i);
-                        result[i]=jsonObject.getString("name");
-                    }
-                    return  result;
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                return null;
             }
-
-
         });
 
+    }
+    private String[] parseJSONObject(String responseText) {
+        JSONArray jsonArray= null;
+        try {
+            jsonArray = new JSONArray(responseText);
+            String[] result=new String[jsonArray.length()];
+            for (int i=0;i<jsonArray.length();i++){
+                JSONObject jsonObject=jsonArray.getJSONObject(i);
+                this.data[i]=jsonObject.getString("name");
+            }
+            return  result;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
